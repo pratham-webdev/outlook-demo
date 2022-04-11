e16746d52de7c2c6e2a5891c34a1939afb9b1fe8
Pratham <pratham.sethi@mitratech.com>
2022-04-11 15:51:28 +0530

    working quick navigation

--- js/quick-navigation.js
+++ js/quick-navigation.js
@@ -2,87 +2,103 @@ const quickNavItems = [{
     id: 1,
     name: `Documents`,
     padding: 'ps-3',
+    level: `main`,
     items: [
         {
             id: 'd-1',
             name: 'Emails',
             padding: 'ps-4',
+            level: `level1`,
+            parentClass: "",
+            childClass: "",
+            aria: "false",
             items: [
                 {
                     id: 'de-1',
                     name: 'Notes',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`
                 },
                 {
                     id: 'de-2',
                     name: 'Recordings',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`
                 },
                 {
                     id: 'de-3',
                     name: 'Maps',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`
 
                 },
                 {
                     id: 'de-4',
                     name: 'Discussions',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`
 
                 },
                 {
                     id: 'de-5',
                     name: 'Spreadsheets',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`
 
                 },
 
             ],
-            parentClass: "",
-            childClass: "",
-            aria: "false",
         },
         {
             id: 'f-1',
             name: 'Files',
             padding: 'ps-4',
+            level: `level1`,
+            parentClass: "",
+            childClass: "",
+            aria: "false",
             items: [
                 {
                     id: 'fe-1',
                     name: 'Notes',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
                 },
                 {
                     id: 'fe-2',
                     name: 'Recordings',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
                 },
                 {
                     id: 'fe-3',
                     name: 'Maps',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
                 {
                     id: 'fe-4',
                     name: 'Discussions',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
                 {
                     id: 'fe-5',
                     name: 'Spreadsheets',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
 
@@ -92,173 +108,197 @@ const quickNavItems = [{
             id: 'p-1',
             name: 'Presentations',
             padding: 'ps-4',
+            level: `level1`,
+            parentClass: "",
+            childClass: "",
+            aria: "false",
             items: [
                 {
                     id: 'pe-1',
                     name: 'Notes',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
                 },
                 {
                     id: 'pe-2',
                     name: 'Recordings',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
                 },
                 {
                     id: 'pe-3',
                     name: 'Maps',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
                 {
                     id: 'pe-4',
                     name: 'Discussions',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
                 {
                     id: 'pe-5',
                     name: 'Spreadsheets',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
             ],
-            parentClass: "",
-            childClass: "",
-            aria: "false",
         },
         {
             id: 'c-1',
             name: 'Case Files',
             padding: 'ps-4',
+            level: `level1`,
+            parentClass: "",
+            childClass: "",
+            aria: "false",
             items: [
                 {
                     id: 'ce-1',
                     name: 'Notes',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
                 },
                 {
                     id: 'ce-2',
                     name: 'Recordings',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
                 },
                 {
                     id: 'ce-3',
                     name: 'Maps',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
                 {
                     id: 'ce-4',
                     name: 'Discussions',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
                 {
                     id: 'ce-5',
                     name: 'Spreadsheets',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
             ],
-            parentClass: "",
-            childClass: "",
-            aria: "false",
         },
         {
             id: 'm-1',
             name: 'Medical Records',
             padding: 'ps-4',
+            level: `level1`,
+            parentClass: "",
+            childClass: "",
+            aria: "false",
             items: [
                 {
                     id: 'me-1',
                     name: 'Notes',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
                 },
                 {
                     id: 'me-2',
                     name: 'Recordings',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
                 },
                 {
                     id: 'me-3',
                     name: 'Maps',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
                 {
                     id: 'me-4',
                     name: 'Discussions',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
                 {
                     id: 'me-5',
                     name: 'Spreadsheets',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
             ],
-            parentClass: "",
-            childClass: "",
-            aria: "false",
         },
         {
             id: 'r-1',
             name: 'Research',
             padding: 'ps-4',
+            level: `level1`,
+            parentClass: "",
+            childClass: "",
+            aria: "false",
             items: [
                 {
                     id: 're-1',
                     name: 'Notes',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
                 },
                 {
                     id: 're-2',
                     name: 'Recordings',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
                 },
                 {
                     id: 're-3',
                     name: 'Maps',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
                 {
                     id: 're-4',
                     name: 'Discussions',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
                 {
                     id: 're-5',
                     name: 'Spreadsheets',
                     padding: 'ps-1',
-                    files:true,
+                    files: true,
+                    level: `level2`,
 
                 },
             ],
-            parentClass: "",
-            childClass: "",
-            aria: "false",
         },
     ],
     parentClass: "",
@@ -269,30 +309,31 @@ const quickNavItems = [{
 
 const quickFiles = [
     {
-        id:1,
-        name:'meeting notes.txt',
-        icon:'fa-file-lines',
-        docs:false,
+        id: 1,
+        name: 'meeting notes.txt',
+        icon: 'fa-file-lines',
+        docs: false,
     },
     {
-        id:2,
-        name:'recap.docx',
-        icon:'fa-file-word',
-        docs:false,
+        id: 2,
+        name: 'recap.docx',
+        icon: 'fa-file-word',
+        docs: false,
     },
     {
-        id:3,
-        name:'deck.ppt',
-        icon:'fa-file-powerpoint',
-        docs:false,
+        id: 3,
+        name: 'deck.ppt',
+        icon: 'fa-file-powerpoint',
+        docs: false,
     },
     {
-        id:4,
-        name:'invoices.xlsx',
-        icon:'fa-file-excel',
-        docs:false,
+        id: 4,
+        name: 'invoices.xlsx',
+        icon: 'fa-file-excel',
+        docs: false,
     }
 ]
+
 
 function leftNavTemplate(arr) {
     return `<nav class="bd-links pt-3 pb-3 px-lg-3 px-md-2 px-sm-2 px-2">${createNavList(arr)}</nav>`;
@@ -300,7 +341,7 @@ function leftNavTemplate(arr) {
 
 function createNavListTemplate(el) {
     return `<div class="mb-2">
-    <button class="btn d-flex align-items-center rounded w-100 ${el.parentClass}" data-bs-toggle="collapse" aria-expanded=${el.aria} data-bs-target="#quickNav-${el.id}">${el.name}</button>
+    <div class="hover d-flex align-items-center rounded w-100 "><button class="btn ${el.parentClass}" data-bs-toggle="collapse" aria-expanded=${el.aria} data-bs-target="#quickNav-${el.id}"></button><div class="dblname ${el.level}">${el.name}</div></div>
     <div id="quickNav-${el.id}" class="collapse ${el.childClass} ${el.padding}">
     ${el.items ? createNavList(el.items) : ''}
     ${el.files == true ? createNavListItems(quickFiles) : ''}
@@ -310,7 +351,7 @@ function createNavListTemplate(el) {
 
 function createNavListItems(items) {
     let tempItemArr = items.map((item) => {
-            return `<a href="" class="d-block fw-normal pb-1 small rounded"><i class="fa-solid ${item.icon} accent-color me-2"></i>${item.name}</a>`;
+        return `<a href="" class="d-block fw-normal pb-1 small rounded"><i class="fa-solid ${item.icon} accent-color me-2"></i>${item.name}</a>`;
     });
     return tempItemArr.join("");
 }
@@ -322,3 +363,23 @@ function createNavList(arr) {
 
 
 $('#quick-nav-panel').append(leftNavTemplate(quickNavItems));
+
+$('.dblname').on('dblclick', function(){
+    console.log('fired')
+    folderLevel = 0; 
+    $('#doc-title,#bread-crumb').remove(); 
+    createMattersDetails(); 
+    createLevel1('Documents');
+    if($(this).hasClass('level1')){
+        let text = $(this).text();
+        createLevel2(text);
+    }
+    else if($(this).hasClass('level2')){
+        let text = $(this).text();
+        let parentText = $(this).parents('.collapse').prev('.hover').find('.level1').text();
+        createLevel2(parentText);
+        createLevel3(text);
+    }
+    $('.bd-links .active').removeClass('active');
+    $(this).parent().addClass('active');
+})
