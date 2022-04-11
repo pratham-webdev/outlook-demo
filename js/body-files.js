148cfab2bf3e4018473fe63ad701284f0223abe6
Pratham <pratham.sethi@mitratech.com>
2022-04-11 20:07:31 +0530

    div + css changes for going back to matters list from doc title

---
+++ js/body-files.js
@@ -0,0 +1,55 @@
+const bodyFiles = [
+    {
+        id:'bf-1',
+        name:'meeting notes.txt',
+        icon:'fa-file-lines',
+        docs:false,
+        func:'JavaScript:void(0);'
+    },
+    {
+        id:'bf-2',
+        name:'recap.docx',
+        icon:'fa-file-word',
+        docs:false,
+        func:'JavaScript:void(0);'
+    },
+    {
+        id:'bf-3',
+        name:'deck.ppt',
+        icon:'fa-file-powerpoint',
+        docs:false,
+        func:'JavaScript:void(0);'
+    },
+]
+
+function bodyFilesTemplate(el){
+    return  `<div class="card m-2">
+      <div class="p-2 d-flex">
+          <div class="icon"><i class="fs-3 fa-solid ${el.icon} accent-color opacity-75"></i></div>
+          <div class="card-details ps-3">
+              <div class="card-name" onClick="docMove = true; ${el.func}; ">${el.name}</div>
+              <div class="text-muted">
+                  <span>Modified by John Smith</span><span class="ms-2">•</span><span
+                      class="ms-2">3/22/21 1:02 PM </span>
+              </div>
+             ${el.docs == true ? createIndiFiles() : ''} 
+          </div>
+      </div>
+  </div>`
+  }
+  
+  function createIndiFiles(){
+      return `<div class="text-muted">
+      <a class="btn btn-sm btn-new p-0" href="#">Documents</a><span class="ms-2">•</span><a
+          class="ms-2 btn btn-sm btn-new p-0" href="#">Appointments</a>
+  </div>`
+  }
+
+let bodyFilesArr = bodyFiles.map(el => bodyFilesTemplate(el)).join("");
+
+
+function createBodyFiles(name){
+    $('#body-files').append(bodyFilesArr);
+}
+
+createBodyFiles();
--- js/doc-title.js
+++ js/doc-title.js
@@ -1,9 +1,9 @@
 
 
 const documentTitle = `  <div id="doc-title" class="d-flex align-items-center">
-<button id="back-icon" class="btn btn-new ms-2 p-1" onclick="upDown()"><i class="fs-5 fa-solid fa-arrow-left"></i></button>
-<div class="ms-2"><i class="fs-5 fa-solid fa-folder opacity-50"></i></div>
-<div class="ms-2">Q10834534 - The State vs Isabella</div>
+<button id="back-icon" class="btn btn-new mx-2 p-1" onclick="upDown()"><i class="fs-5 fa-solid fa-arrow-left"></i></button>
+<div class="doc-title-header p-1" onclick="folderLevel=0; createMatters();"><i class="fs-5 fa-solid fa-folder opacity-50 me-2"></i>
+Q10834534 - The State vs Isabella</div>
 </div>
 <div id="bread-crumb" class="px-2 d-flex"></div>`
 
