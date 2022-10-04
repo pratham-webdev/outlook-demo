4e2a5f832c6cf4b31a5631e44813e844a35b327a
Pratham <pratham.sethi@mitratech.com>
2022-10-04 17:41:59 +0530

    quick navigation fix for matter name

--- js/matter-detail.js
+++ js/matter-detail.js
@@ -16,10 +16,14 @@ const mattersDetails = [
 
 let mattersDetailsArr = mattersDetails.map(el => cardTemplate(el)).join("");
 
+let tempName;
+
 function createMattersDetails(name){
+    //setting name
+    name != undefined ? tempName = name : '';
     //disabling matter icon
     $('#create-matter-icon-button').attr('disabled', true);
-    folderLevel == 0 ? createDocumentTitle(name) : ''; // sending name property for document title container
+    folderLevel == 0 ? createDocumentTitle(tempName) : ''; // sending name property for document title container
     $('#card-container').empty();
     $('#card-container').append(mattersDetailsArr);
     $('#quick-nav-button').show();
